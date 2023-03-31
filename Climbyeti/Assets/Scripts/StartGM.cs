01a900847fe4b20b005a89b76d6339355edab9fd
HaJaeHeon <gkhen@naver.com>
2023-04-01 05:37:33 +0900

    StartScene UI add
    11

--- Climbyeti/Assets/Scripts/StartGM.cs
+++ Climbyeti/Assets/Scripts/StartGM.cs
@@ -1,15 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+public enum ActivePanel
+{
+    TitlePanel = 0,
+    HTPanel = 1
+}
 public class StartGM : MonoBehaviour
 {
+    public ActivePanel pan = ActivePanel.TitlePanel;
+    public GameObject[] panels;
+    
+    private void Start()
+    {
+     ChangePanel(ActivePanel.TitlePanel );   
+    }
+
     [ContextMenu("HTP")]
     public void ClickHtpButton()
     {
-        
+        ChangePanel(ActivePanel.HTPanel);
+    }
+
+    [ContextMenu("HTP_Close")]
+    public void ClickHTP_CloseButton()
+    {
+        ChangePanel(ActivePanel.TitlePanel);
+    }
+
+    private void ChangePanel(ActivePanel pan)
+    {
+        foreach (GameObject _panel in panels)
+        {
+            _panel.SetActive(false);
+        }
+        panels[(int)pan].SetActive(true);
     }
 
     [ContextMenu("Start")]
